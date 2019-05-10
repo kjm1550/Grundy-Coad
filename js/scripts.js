1f6ac15f816f8fd974a419a0108ada4543047994
Kyle <37756806+kjm1550@users.noreply.github.com>
2019-05-09 22:08:17 -0500

    Created content for the services page.
    Added services and links to the service page. Still need to do the want to help service tab. Styled the service page. Added pictures and pdf that will be on or linked in the service page.

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@ $('#toggle').click(function() {
 	$(this).toggleClass('active');
 	$('#overlay').toggleClass('open');
 });
+
+/* Back up vanila JavaScript
+function funcName() {
+	var element = document.getElementById('toggle');
+	element.classList.toggle('active');
+	var element = document.getElementById('overlay');
+	element.classList.toggle('open');
+}
+*/
